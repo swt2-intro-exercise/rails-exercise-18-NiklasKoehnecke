05dd21891579cbb1d0e5c6fb939bbe5ab0c2f752
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2018-10-30 19:01:25 +0100

    Failing test for #4

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :author do
-    first_name { "MyString" }
-    last_name { "MyString" }
+    first_name { "Alan" }
+    last_name { "Turing" }
     homepage { "MyString" }
   end
 end
---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "show author page", type: :feature do
+
+  it "should render withour error" do
+    visit author_path(@alan)
+  end
+
+  it "should show the name of the author" do
+    visit author_path(@alan)
+    expect(page).to have_text(@alan.first_name)
+    expect(page).to have_text(@alan.last_name)
+    expect(page).to have_text(@alan.homepage)
+  end
+end
