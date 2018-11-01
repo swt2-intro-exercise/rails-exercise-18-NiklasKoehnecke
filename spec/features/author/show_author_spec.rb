0f6a1ddff21ae63c444ee9cb3b303f890dd08e54
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2018-11-01 17:34:19 +0100

    finally fixed the routing id problem

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe "show author page", type: :feature do
 
   it "should render withour error" do
-    visit author_path(@alan)
+    visit authors_path(@alan)
   end
 
   it "should show the name of the author" do
-    visit author_path(@alan)
+    visit authors_path(@alan)
     expect(page).to have_text(@alan.first_name)
     expect(page).to have_text(@alan.last_name)
     expect(page).to have_text(@alan.homepage)
