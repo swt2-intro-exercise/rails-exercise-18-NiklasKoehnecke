20ce7649f6078fcb408430bfdd2dcca1aea15ae4
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2018-11-04 15:07:21 +0100

    Working implementation for #5

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,20 +2,22 @@ describe 'index auther page' do
 
   it 'should have a table' do
     visit authors_path
-    expect(page).to have_table '#authors_table'
+    expect(page).to have_table
   end
 
-  it 'should have full names of authors' do
-    @alan = FactoryBot.create :author
-    @alan.save
+  it 'should have full names and emails of authors' do
+    @temp_authors = Author.all
     visit authors_path
-    expect(page).to have_text @alan.name
-    expect(page).to have_link @alan.homepage
+    @temp_authors.each do |author|
+      expect(page).to have_text author.name
+      expect(page).to have_link author.homepage
+      expect(page).to have_link link_to 'Show', author_path(author)
+    end
   end
 
   it 'should have a link to add new authors' do
     visit authors_path
-    expect(page).to have_link 'New', href: new_authors_path
+    expect(page).to have_link 'New', href: new_author_path
   end
 
 end
