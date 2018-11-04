982a44179f8acac18354ff5804883b0c0929eaee
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2018-11-04 16:06:42 +0100

    Failing test for #7

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -7,10 +7,14 @@ describe "New author page", type: :feature do
   end
 
   it "should have text inputs for an author's first name, last name, and homepage" do
-
     # these are the standard names given to inputs by the form builder
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
+  end
+
+  it "should throw errors when saving invalid authors" do
+    find('input[name="commit"]').click
+    expect(page).to have_text('error/i')
   end
 end
