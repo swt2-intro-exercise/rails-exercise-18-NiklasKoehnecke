4e6430872606174797fdb1540fb5ae1616eedee3
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2018-11-04 16:25:21 +0100

    Failing test for #8

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
-describe "show author page", type: :feature do
-
+describe "edit author page" do
   before :each do
     @alan = FactoryBot.create :author
-    visit author_path(@alan)
+    visit edit_author_path(@alan)
   end
 
-  it "should render withour error" do
+  it "should render without error" do
   end
 
-  it "should show the name of the author" do
+  it "should display the same information as the show page" do
     expect(page).to have_text(@alan.first_name)
     expect(page).to have_text(@alan.last_name)
     expect(page).to have_text(@alan.homepage)
