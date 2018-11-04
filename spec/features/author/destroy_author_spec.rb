bb9f48a1bd6df7df9dcd0ff61a7123a526597793
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2018-11-04 22:49:23 +0100

    Failing test for #12

--- spec/features/author/destroy_author_spec.rb
+++ spec/features/author/destroy_author_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 describe "destroy route" do
 
-  let(:action) { :destroy }
-  let(:params) {   }
-  let(:referer) { '/' }
-
+  let(:valid_session) {{}}
   it "should delete the author from the database" do
     @author = FactoryBot.create :author
     @author.save
     expect(Author.exists?(@author.id)).to be(true)
+    #delete :destroy, params: {id: @author.to_param}, session: valid_session
     #TODO SOMETHING THAT WORKS
     #expect(Author.exists?(@author.id)).to be(false)
   end
